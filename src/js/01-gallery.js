42bce5fcb325aa60c26423206a126f2852dd5a38
Eugene8615 <reznichenko8615@gmail.com>
2022-12-25 20:14:06 +0200

    2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,6 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-import SimpleLightbox from "simplelightbox";
+import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm"
 // Change code below this line
 
 console.log(SimpleLightbox);
