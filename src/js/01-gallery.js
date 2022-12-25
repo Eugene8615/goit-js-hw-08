4849f49460a3fba3e6d4f0eede930896d039394f
Eugene8615 <reznichenko8615@gmail.com>
2022-12-25 20:48:43 +0200

    4

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,19 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
-console.log(SimpleLightbox);
 
+import { galleryItems } from './gallery-items.js';
+
+const galleryListREF = document.querySelector('.gallery');
+galleryListREF.innerHTML = crateMarkup(galleryItems);
+
+function crateMarkup(items) {
+    return items.map(item => `<div><a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a></div>`).join('')
+};
+
+var lightbox = new SimpleLightbox('.gallery a', { captionsData: `alt`, captionDelay: 250,});
